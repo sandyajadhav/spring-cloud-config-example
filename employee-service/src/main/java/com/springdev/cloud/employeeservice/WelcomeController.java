5004d2a4e4affe5073d7f43d942e47383c3e2fc3
sjadhav <sjadhav@ancestry.com>
2019-03-14 12:50:19 -0700

    updated propeties file for testing

--- employee-service/src/main/java/com/springdev/cloud/employeeservice/WelcomeController.java
+++ employee-service/src/main/java/com/springdev/cloud/employeeservice/WelcomeController.java
@@ -9,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class WelcomeController
 {
+    public  WelcomeController(){
+        System.out.println("sldfjsldk");
+    }
     @Value("${app.service-name}")
     private String serviceName;
 
